d2ab0b496506f2104b89a829a03aa92a44eedb88
Gustavo Vilela Mitraud <gus_vila_mitra@hotmail.com>
2022-11-09 14:55:39 -0300

    gramatica

--- quebra_senhas.py
+++ quebra_senhas.py
@@ -4,7 +4,7 @@ def codificar_senha(senha):
     senha_encoded = senha.encode("utf-8")
     digest = hashlib.sha512(senha_encoded).digest()
     digest_b64_encoded = base64.b64encode(digest)
-    digest_b64_encoded_utf8_decoded = digest_b64_encoded.decode("uft-8")
+    digest_b64_encoded_utf8_decoded = digest_b64_encoded.decode("utf-8")
     return digest_b64_encoded_utf8_decoded
 
 
